d921e3ed2b6aa73a17f8c250e1b5d84caf062607
Sampreet Singh <preet.samsam@gmail.com>
2025-10-23 16:41:46 +0530

    chore: add placeholder API routes and index.d.ts with TODO notes

---
+++ src/app/api/generate/route.ts
@@ -0,0 +1,1 @@
+/** TODO: Make API route to communicate with Gemini AI and send back AI response */
---
+++ src/app/api/sitemap.xml/route.ts
@@ -0,0 +1,1 @@
+/** TODO: Make API route to generate a dynamic sitemap and return to search engine crawlers and browsers */
